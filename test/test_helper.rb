0a75cf11e617dae42496f52b5e5d203395a9a1c1
Jeff Lewis <lewisj88@mac.com>
2015-05-30 08:30:25 -0700

    Solution to Exercise 1

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,5 +8,13 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  def setup
+    @title = "| Ruby on Rails Tutorial Sample App"
+  end
+
+  def teardown
+    @title = nil
+  end
+
   # Add more helper methods to be used by all tests here...
 end
